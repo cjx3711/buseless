98ef2eaadca8b6e66975856ab58eb664588cf4bd
Jia Xun <chaijiaxun@gmail.com>
2017-01-22 02:54:26 +0800

    Added bus stop switch

--- public/angular/controllers/busstop.js
+++ public/angular/controllers/busstop.js
@@ -1,22 +1,39 @@
-app.controller('busstopController', ['$scope', '$location', 'dataService', '$http', function ($scope, $location, dataService, $http) {
-  console.log("BusStop");
-  	$scope.showSwitch = dataService.selectedStops.length == 2 ? true : false
-  	console.log(dataService.selectedStops);
-  	$scope.selected = dataService.selectedStops[0]["BusStopCode"]
-  	var url = '/nextbus/' + $scope.selected
-    $http.get(url).then(
-      function successCallback(response) {
-        console.log("Server response", response.data)
-          $scope.services = response.data;
-        },
-        function errorCallback(response) {
-          console.log("Server error", response.data);
-        }
-      );
-    $scope.selectService = function(service) {
-    	locat = "/#/service/" + $scope.selected + "/" + service;
-    	console.log(locat)
-    	window.location.href = locat;
+app.controller('busstopController', ['$scope', '$location', 'dataService', '$http', '$routeParams', function ($scope, $location, dataService, $http, $routeParams) {
+    console.log("BusStop");
+    $scope.busStopObject = dataService.getBusStop($routeParams.busstop);
+    console.log($scope.busStopObject);
+    if ( $scope.busStopObject == null ) {
+      $scope.error = true;
+    } else {
+      $scope.error = false;
+      $scope.showSwitch = $scope.busStopObject.Stops.length == 2;
+
+      if ( $scope.showSwitch ) {
+        $scope.switch = $scope.busStopObject.Stops[1].BusStopCode == $routeParams.busstop;
+      }
+
+    	$scope.selected = $scope.busStopObject.Stops[0]["BusStopCode"]
+    	var url = '/nextbus/' + $scope.selected
+      $http.get(url).then(
+        function successCallback(response) {
+          console.log("Server response", response.data)
+            $scope.services = response.data;
+          },
+          function errorCallback(response) {
+            console.log("Server error", response.data);
+          }
+        );
+      $scope.selectService = function(service) {
+      	locat = "/#/service/" + $scope.selected + "/" + service;
+      	console.log(locat)
+      	window.location.href = locat;
+      }
+
+      $scope.switchStop = function() {
+        console.log($scope.switch);
+        window.location.href = "/#/busstop/" + $scope.busStopObject.Stops[$scope.switch?0:1].BusStopCode
+      }
     }
-  
+
+
 }]);

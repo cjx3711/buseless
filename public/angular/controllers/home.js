19869086264be749cd1bf1bea98c8d46a3d8e09e
Jia Xun <chaijiaxun@gmail.com>
2017-01-22 02:54:08 +0800

    Moved bus stop data to service.

--- public/angular/controllers/home.js
+++ public/angular/controllers/home.js
@@ -1,95 +1,6 @@
 app.controller('homeController', ['$scope', '$location', 'dataService', '$http', function ($scope, $location, dataService, $http) {
   console.log("My stuff");
-  $scope.busStopArr = [
-    {
-      "RoadName": "Clementi Ave 1",
-      "Stops": [
-        {
-          "BusStopCode": "16991",
-          "RoadName": "Clementi Ave 1",
-          "Description": "Opp Nan Hua High Sch",
-          "Latitude": 1.30777929773028,
-          "Longitude": 103.76988582423061
-        }
-      ]
-    },
-    {
-      "RoadName": "Clementi Rd",
-      "Stops": [
-        {
-          "BusStopCode": "17091",
-          "RoadName": "Clementi Rd",
-          "Description": "Aft Clementi Ave 1",
-          "Latitude": 1.3090313954164,
-          "Longitude": 103.77153500824593
-        },
-        {
-          "BusStopCode": "17099",
-          "RoadName": "Clementi Rd",
-          "Description": "Aft Dover Rd",
-          "Latitude": 1.30798165844336,
-          "Longitude": 103.77170498369206
-        }
-      ] 
-    },
-    {
-      "RoadName": "C'wealth Ave West",
-      "Stops": [
-        {
-          "BusStopCode": "19031",
-          "RoadName": "C'wealth Ave West",
-          "Description": "Dover Stn",
-          "Latitude": 1.31123416666912,
-          "Longitude": 103.77838361113201
-        },  
-        {
-          "BusStopCode": "19039",
-          "RoadName": "C'wealth Ave West",
-          "Description": "Dover Stn",
-          "Latitude": 1.31167951129602,
-          "Longitude": 103.77868390552867
-        }
-      ]
-    },
-    {
-      "RoadName": "C'wealth Ave West",
-      "Stops": [
-        {
-          "BusStopCode": "19041",
-          "RoadName": "C'wealth Ave West",
-          "Description": "Opp Sch Of Science & Tech",
-          "Latitude": 1.31235972197855,
-          "Longitude": 103.77552750001337
-        },
-        {
-          "BusStopCode": "19049",
-          "RoadName": "C'wealth Ave West",
-          "Description": "Sch Of Science & Tech",
-          "Latitude": 1.31266302174215,
-          "Longitude": 103.77479215922537
-        }
-      ]
-    },
-    {
-      "RoadName": "C'wealth Ave West",
-      "Stops": [    
-        {
-          "BusStopCode": "19051",
-          "RoadName": "Dover Rd",
-          "Description": "New Town Sec Sch",
-          "Latitude": 1.30896444398008,
-          "Longitude": 103.77392333299538
-        },
-        {
-          "BusStopCode": "19059",
-          "RoadName": "Dover Rd",
-          "Description": "University Town",
-          "Latitude": 1.30894500001531,
-          "Longitude": 103.7727225000061
-        }
-      ]
-    }        
-  ];
+  $scope.busStopArr = dataService.busStopArr;
 
 
 
@@ -152,11 +63,10 @@ app.controller('homeController', ['$scope', '$location', 'dataService', '$http',
       });
 
       map.mapTypes.set('mystyle', new google.maps.StyledMapType(myStyle, { name: 'My Style' }));
-      window.marker = new google.maps.Marker({position: {lat: 1.3062, lng: 103.7732}, map: map, icon: iconConfig});
-      window.marker.setLabel('121')
+      // window.marker = new google.maps.Marker({position: {lat: 1.3062, lng: 103.7732}, map: map, icon: iconConfig});
+      // window.marker.setLabel('121')
     }
     window.initMaps();
-
 
     function updateMarker(lat, lng, no, i) {
       if ( i >= markers.length ) {
--- public/angular/service/data.js
+++ public/angular/service/data.js
@@ -14,5 +14,108 @@ app.service('dataService', [ '$http', function($http) {
   // }
   // this.selectedStops = [];
 
+  this.getBusStop = function(id) {
+    var ret = null; // I know this is not efficient.
+    this.busStopArr.forEach( function(busStop) {
+      busStop.Stops.forEach( function(stop) {
+        if ( stop.BusStopCode == id ) {
+          ret = busStop;
+        }
+      });
+    });
+    return ret;
+  }
+  this.busStopArr = [
+    {
+      "RoadName": "Clementi Ave 1",
+      "Stops": [
+        {
+          "BusStopCode": "16991",
+          "RoadName": "Clementi Ave 1",
+          "Description": "Opp Nan Hua High Sch",
+          "Latitude": 1.30777929773028,
+          "Longitude": 103.76988582423061
+        }
+      ]
+    },
+    {
+      "RoadName": "Clementi Rd",
+      "Stops": [
+        {
+          "BusStopCode": "17091",
+          "RoadName": "Clementi Rd",
+          "Description": "Aft Clementi Ave 1",
+          "Latitude": 1.3090313954164,
+          "Longitude": 103.77153500824593
+        },
+        {
+          "BusStopCode": "17099",
+          "RoadName": "Clementi Rd",
+          "Description": "Aft Dover Rd",
+          "Latitude": 1.30798165844336,
+          "Longitude": 103.77170498369206
+        }
+      ]
+    },
+    {
+      "RoadName": "C'wealth Ave West",
+      "Stops": [
+        {
+          "BusStopCode": "19031",
+          "RoadName": "C'wealth Ave West",
+          "Description": "Dover Stn",
+          "Latitude": 1.31123416666912,
+          "Longitude": 103.77838361113201
+        },
+        {
+          "BusStopCode": "19039",
+          "RoadName": "C'wealth Ave West",
+          "Description": "Dover Stn",
+          "Latitude": 1.31167951129602,
+          "Longitude": 103.77868390552867
+        }
+      ]
+    },
+    {
+      "RoadName": "C'wealth Ave West",
+      "Stops": [
+        {
+          "BusStopCode": "19041",
+          "RoadName": "C'wealth Ave West",
+          "Description": "Opp Sch Of Science & Tech",
+          "Latitude": 1.31235972197855,
+          "Longitude": 103.77552750001337
+        },
+        {
+          "BusStopCode": "19049",
+          "RoadName": "C'wealth Ave West",
+          "Description": "Sch Of Science & Tech",
+          "Latitude": 1.31266302174215,
+          "Longitude": 103.77479215922537
+        }
+      ]
+    },
+    {
+      "RoadName": "C'wealth Ave West",
+      "Stops": [
+        {
+          "BusStopCode": "19051",
+          "RoadName": "Dover Rd",
+          "Description": "New Town Sec Sch",
+          "Latitude": 1.30896444398008,
+          "Longitude": 103.77392333299538
+        },
+        {
+          "BusStopCode": "19059",
+          "RoadName": "Dover Rd",
+          "Description": "University Town",
+          "Latitude": 1.30894500001531,
+          "Longitude": 103.7727225000061
+        }
+      ]
+    }
+  ];
+
+
 
 }]);
